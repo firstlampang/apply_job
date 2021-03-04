f3e3197b8f1122a3790760099953414f25d0bcf0
Krisadakorn Chaiwong <>
2021-03-04 17:05:44 +0700

    add to db

--- app/controllers/fitting_controller.rb
+++ app/controllers/fitting_controller.rb
@@ -10,12 +10,12 @@ class FittingController < ApplicationController
     # data_hash = JSON.parse(@file)
     # @question = Question.new(question: data_hash, active: true, version: 6)
     # @question.save
-  # puts "file :: #{@file}"
+    # puts "file :: #{@file}"
 
     # @data_hash = data_hash
 
     @file = Question.first
-    puts @file.question
+    # puts @file.question
     # data_hash = JSON.parse()
 
    
@@ -29,11 +29,12 @@ class FittingController < ApplicationController
 
     @question_id = params[:question_id]
 
-    json = params.permit(:question1, :question2, :question3, :question4, :question5, :question6,
+    json = params.permit(:question1, :question2, :question3, :question4, :question5, :question6, :question7,
         :question8, :question9, :question11, :question14, :question15, :question17, :question18,
-        :question19, :question20, :question21, :question23, :start24, :question25,
+        :question19, :question20, :question21, :question23, :star24, :question25,
         :question26, :question27, :question28, :question29, :question30, :question31, :question32, :question33,
-        :question34, :question35, :start36, :question37, :question38, :question39, :question40, :question41, :question42, question10: [],question12: [], question13: [], question16: [], question22: [])
+        :question34, :question35, :question36, :question37, :question38, :question39, :question40, :question41, :star42,
+        question10: [],question12: [], question13: [], question16: [], question22: [])
     
     puts json
     @answer = Answer.new(answer: json, apply_date: Time.now, name: params[:question1], question_id: @question_id)
